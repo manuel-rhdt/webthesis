b92d9dbd2bd7baa9b56a317f209601f1537da888
Manuel Reinhardt <mail@manuel-rhdt.de>
2026-01-16 17:50:33 +0100

    fix figures

--- markdown_preprocess.py
+++ markdown_preprocess.py
@@ -54,13 +54,28 @@ def main():
         replacement = r'\2 {#\1}'
         new_content = re.sub(pattern, replacement, new_content, flags=re.MULTILINE)
 
-        pattern = r'<embed src="([^"]+)\.pdf" />'
-        replacement = r'<img src="figures/\1.svg" />'
+        pattern = r'<embed\s+src="([^"]+)\.pdf"[^>]*>'
+        replacement = r'<img src="\1.svg" />'
         new_content = re.sub(pattern, replacement, new_content)
 
-        pattern = r'<figure id="([^"]+)">(.+?)</figure>'
-        replacement = r'::: {#\1}\n<figure>\2</figure>\n:::'
-        new_content = re.sub(pattern, replacement, new_content, flags=re.DOTALL)
+        # Convert HTML figures to Pandoc markdown figures
+        pattern = r'<figure[^>]*id=["\']([^"\']+)["\'][^>]*>((.|\n)*?)</figure>'
+        def replace_figure(match):
+            identifier = match.group(1)
+            content = match.group(2)
+            img_match = re.search(r'<img[^>]+src=["\'](.*?)["\']', content, re.IGNORECASE)
+            img_src = img_match.group(1) if img_match else None
+            
+            img_src = "figures/" + Path(img_src).name
+
+            # Extract figcaption if present
+            caption_match = re.search(r'<figcaption[^>]*>((.|\n)*?)</figcaption>', content, re.DOTALL | re.IGNORECASE)
+            caption = caption_match.group(1).strip() if caption_match else None
+
+            # create markdown figure
+            return f"![{caption}]({img_src}){{#{identifier}}}"
+
+        new_content = re.sub(pattern, replace_figure, new_content, re.DOTALL | re.IGNORECASE)
 
         # Save each file at ./ directory
         output_file = Path('.') / md_file.name
